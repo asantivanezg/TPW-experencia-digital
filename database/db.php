b37e17bdb56dab6ab5778135a329030d15f32de7
Axel Santivañez <asantivanezg@hotmail.com>
2022-07-13 16:43:43 -0500

    refactoring and cleaning

---
+++ database/db.php
@@ -0,0 +1,9 @@
+<?php
+
+$usuario = "root";
+$password = "admin";
+$database = "bd_experiencia_Digital";
+
+$conexion = mysqli_connect("localhost", $usuario, $password, $database) or die("error de conexion");
+
+?>
