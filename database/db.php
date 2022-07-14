697c45f666b740a357048a91dba1a299e41e3721
Axel Santivañez <asantivanezg@hotmail.com>
2022-07-14 04:54:43 -0500

    refactor & fixed database conexion

--- database/db.php
+++ database/db.php
@@ -1,9 +1,16 @@
 <?php
+class Database
+{
+    public static function Conectar()
+    {
+        $usuario = "root";
+        $password = "admin";
+        $database = "bd_experiencia_Digital";
 
-$usuario = "root";
-$password = "admin";
-$database = "bd_experiencia_Digital";
+        $pdo = new PDO("mysql:host=localhost; dbname=".$database, $usuario, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$conexion = mysqli_connect("localhost", $usuario, $password, $database) or die("error de conexion");
-
+        return $pdo;
+    }
+}
 ?>
---
+++ src/models/producto.php
@@ -0,0 +1,21 @@
+<?php
+include("database/db.php");
+
+class Producto {
+
+    private $pdo;
+
+    
+    public function __construct()
+    {
+      
+        try {
+            $this -> pdo =
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
+}
+
+?>
