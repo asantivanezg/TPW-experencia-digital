55990f7761c9290cbbf1e2c37f94dfe0bbbc3a4d
Axel Santivañez <asantivanezg@hotmail.com>
2022-07-14 04:55:07 -0500

    fixed database conexion

--- database/db.php
+++ database/db.php
@@ -7,7 +7,7 @@ class Database
         $password = "admin";
         $database = "bd_experiencia_Digital";
 
-        $pdo = new PDO("mysql:host=localhost; dbname=".$database, $usuario, $password);
+        $pdo = new PDO("mysql:host=localhost; dbname=" . $database, $usuario, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         return $pdo;
