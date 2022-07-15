51d729495b5e421677a6e57927a600243ac8a635
Peterpacheco234 <pachecopeter137@gmail.com>
2022-07-15 00:39:57 -0500

    cambio 7

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	<meta name="keywords" content="">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
 		integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/IMG/main">
+	<link rel="stylesheet" href="styl">
 	<link rel="stylesheet" href="style/redes-sociales.css">
 	<script async>(function (w, d) {
 			var h = d.head || d.getElementsByTagName("head")[0];
